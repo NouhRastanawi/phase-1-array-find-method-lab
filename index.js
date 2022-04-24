4b613685f72f28c80a1faf14e062b67e0ce3620d
NouhRastanawi <noahrastdark@gmail.com>
2022-04-24 13:16:15 +0300

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+function isWin(el){
+    if(el.result === 'W') return el;
+}
+
+const superbowlWin = (arr) => {
+    const value = arr.find(isWin);
+    if(typeof(value) === 'undefined') {
+        return undefined
+    }
+    else{
+        return value.year;
+    }
+};
+console.log(superbowlWin(sadReality))
